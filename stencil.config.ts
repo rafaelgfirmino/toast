3d0464fbe8bc32c66e4dac0e21e5288d0fcb4525
Rafael Firmino <rafaelgfirmino@gmail.com>
2020-11-08 23:46:31 -0300

    Change the build process

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,13 +1,15 @@
 import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
-
 export const config: Config = {
-  namespace: 'ctt-toast',
+  namespace: 'toast',
   globalStyle: './src/assets/toast.scss',
   outputTargets: [
     {
       type: 'dist',
       esmLoaderPath: '../loader',
+      copy: [
+        { src: './assets/icons', dest: './assets/icons' }
+      ]
     },
     {
       type: 'dist-custom-elements-bundle',
